a8d8032213899a1adc2af57855ee3c662f23f482
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2025-09-26 14:57:39 +0700

    Init project

---
+++ src/main/java/com/sajikitchen/saji_cashier/SajiCashierApplication.java
@@ -0,0 +1,13 @@
+package com.sajikitchen.saji_cashier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajiCashierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SajiCashierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sajikitchen/saji_cashier/SajiCashierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajikitchen.saji_cashier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SajiCashierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
