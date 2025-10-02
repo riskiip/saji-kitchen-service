2836193c0b23fe3e5915a1ae67953e486efc9494
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2025-10-02 16:35:57 +0700

    fix : Change allowed origins

--- src/main/java/com/sajikitchen/saji_cashier/config/SecurityConfig.java
+++ src/main/java/com/sajikitchen/saji_cashier/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Alamat frontend
+        configuration.setAllowedOrigins(List.of("https://saji-kitchen-management.vercel.app")); // Alamat frontend
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
